99e3a7e5844e4f80aadbb0dee667334bbeaa036c
houheshuai <903491912@qq.com>
2017-09-05 10:07:48 +0800

    HouHeShuai add AndroidAnQuanWeiShi

---
+++ AndroidAnQuanWeiShi/src/com/itheima52/mobilesafe/activity/AToolsActivity.java
@@ -0,0 +1,33 @@
+package com.itheima52.mobilesafe.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.itheima52.mobilesafe.R;
+
+/**
+ * 高级工具
+ * 
+ * @author Kevin
+ * 
+ */
+public class AToolsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_atools);
+	}
+
+	/**
+	 * 归属地查询
+	 * 
+	 * @param view
+	 */
+	public void numberAddressQuery(View view) {
+		startActivity(new Intent(this, AddressActivity.class));
+	}
+
+}
